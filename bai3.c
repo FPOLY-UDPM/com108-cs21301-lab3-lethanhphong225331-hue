6baaaab1384a6686ba68e6ad757501d2e4c3dd83
lethanhphong225331-hue <lethanhphong225331@gmail.com>
2026-01-17 09:00:53 +0700

    lab3

--- bai3.c
+++ bai3.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [lê Thành Phong]
+ * MSSV:      [PS47422]
+ * Lớp:       [CS21302]
  *****************************************************************************/
 
 //  BÀI 3: XÂY DỰNG CHƯƠNG TRÌNH TÍNH TIỀN ĐIỆN 
@@ -13,11 +13,24 @@
 int main(){
     
     // Khai báo biến
-
+    int soDien;
+    float soTien;
 
     // Nhập dữ liệu
-
+    printf("Nhap so dien tieu thu: ");
+    scanf("%d", &soDien);
 
     // Xử lý, tính toán VÀ Hiển thị kết quả
+    if (soDien <= 50) {
+        soTien = soDien * 1000;
+    } else if (soDien <= 100) {
+        soTien = 50 * 1000 + (soDien - 50) * 1200;
+    } else if (soDien <= 200) {
+        soTien = 50 * 1000 + 50 * 1200 + (soDien - 100) * 1500;
+    } else {
+        soTien = 50 * 1000 + 50 * 1200 + 100 * 1500 + (soDien - 200) * 250;
+    }
 
+    printf("So tien phai tra la: %.2f\n", soTien);
+    return 0;
 }
